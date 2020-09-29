8ac200adf9d2232f7b56fcf052b48ceac937e9be
gml-sync <example@gmail.com>
2020-09-29 18:29:10 +0300

    1

--- predict.py
+++ predict.py
@@ -12,7 +12,7 @@ def predict(pipe, prefix, batch_size = 8, resize = None):
 	prefix = prefix + '_sintel'
 	if not os.path.exists(prefix):
 		os.mkdir(prefix)
-	
+
 	flo = sintel.Flo(1024, 436)
 
 	for div in ('test',):
@@ -33,12 +33,13 @@ def predict(pipe, prefix, batch_size = 8, resize = None):
 					os.mkdir(seq_output_folder)
 				flo.save(flow, os.path.join(seq_output_folder, fname.replace('.png', '.flo')))
 
+
 	'''
-	KITTI 2012: 
+	KITTI 2012:
 	Submission instructions: For the optical flow benchmark, all flow fields of the test set must be provided in the root directory of a zip file using the file format described in the readme.txt (16 bit color png) and the file name convention of the ground truth (000000_10.png, ... , 000194_10.png).
 
 	KITTI 2015:
-	Submission instructions: Provide a zip file which contains the 'disp_0' directory (stereo), the 'flow' directory (flow), or the 'disp_0', 'disp_1' and 'flow' directories (scene flow) in its root folder. Use the file format and naming described in the readme.txt (000000_10.png,...,000199_10.png). 
+	Submission instructions: Provide a zip file which contains the 'disp_0' directory (stereo), the 'flow' directory (flow), or the 'disp_0', 'disp_1' and 'flow' directories (scene flow) in its root folder. Use the file format and naming described in the readme.txt (000000_10.png,...,000199_10.png).
 	'''
 
 	kitti_resize = (512, 1152) if resize is None else resize
@@ -64,4 +65,3 @@ def predict(pipe, prefix, batch_size = 8, resize = None):
 			pred[:, :, 2] = (64.0 * (flow[:, :, 0] + 512)).astype(np.uint16)
 			pred[:, :, 1] = (64.0 * (flow[:, :, 1] + 512)).astype(np.uint16)
 			cv2.imwrite(out_name, pred)
-			
